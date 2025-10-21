2ddcd5e4703de18756c9cc554a1c58c8a69c884e
ddouglss <ddouglss1999@gmail.com>
2025-10-21 17:21:49 -0300

    Add project files.

---
+++ Ollama/Program.cs
@@ -0,0 +1,20 @@
+﻿using OllamaSharp;
+
+var url = new Uri("http://localhost:11434");
+var client = new OllamaApiClient(url);
+
+
+var models = await client.ListLocalModelsAsync();
+
+client.SelectedModel = "phi3";
+
+
+var chat = new Chat(client);
+var prompt = Console.ReadLine() ?? string.Empty;
+
+await foreach (var answer in chat.SendAsync(prompt))
+    Console.Write(answer);
+
+
+Console.WriteLine();
+Console.WriteLine("---");
